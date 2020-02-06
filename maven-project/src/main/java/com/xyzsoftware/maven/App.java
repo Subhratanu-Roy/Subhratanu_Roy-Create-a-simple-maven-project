3919e1432bee8ed0e87f5f74cc8690b3a08774df
Subhratanu-Roy <subhratanu15@gmail.com>
2020-02-06 20:37:44 +0530

    This is my commit

---
+++ maven-project/src/main/java/com/xyzsoftware/maven/App.java
@@ -0,0 +1,13 @@
+package com.xyzsoftware.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
